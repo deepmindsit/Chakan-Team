69843c7e6992a02a07090d9d5453ce15c8c33a20
mangeshG07 <mangeshg@deepmindsit.com>
2026-03-27 13:16:27 +0530

    android icon

--- lib/utils/image_path.dart
+++ lib/utils/image_path.dart
@@ -1,6 +1,6 @@
 class Images {
-  static const logo = 'assets/images/chakan_logo_new.png';
-  static const logoWhite = 'assets/images/chakan_logo_new_white.png';
+  static const logo = 'assets/images/chakan_seva_logo.png';
+  static const logoWhite = 'assets/images/chakan_seva_white.png';
   static const fevicon = 'assets/images/chakan_fevicon.png';
   static const noInternet = 'assets/images/no_internet_connection.png';
 }
